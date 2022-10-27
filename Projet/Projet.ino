055b79844e81412991b0eb08b7e299e2742190e8
wxssym <wassim.sisaid@gmail.com>
2022-10-27 12:52:56 +0200

    ''changes on project.ino''

--- Projet/Projet.ino
+++ Projet/Projet.ino
@@ -1,5 +1,6 @@
 //definition des inputs et outputs
 #include <U8g2lib.h>
+#include "Wire.h"
 //#include <Adafruit_GFX.h>
 //#include <Adafruit_SSD1327.h>
 
@@ -9,7 +10,19 @@
 int sensibilite = 256;
 
 #define OLED_RESET -1
+unsigned int tab32[32][32];
 
+void tableau48() {
+  for (int  i = 0; i < 31; i++) {
+    for (int  j = 0; j < 31; j++) {
+      if ( i % 2 == 0 || j % 2 == 0 ) {
+        tab32[i][j] = 0;
+      } else {
+        tab32[i][j] = random(1, 16);
+      }
+    }
+  }
+}
 
 
 U8G2_SSD1327_SEEED_96X96_F_SW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
@@ -17,11 +30,27 @@ U8G2_SSD1327_SEEED_96X96_F_SW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ S
 //Adafruit_SSD1327 display(96, 96, &Wire, OLED_RESET, 1000000);
 //U8X8_SSD1327_SEEED_96X96_SW_I2C u8x8(A5, A4);
 
+void displaying() {
+  display.clearBuffer();          // clear the internal memory
+  for (int  i = 0; i < 31; i++) {
+    for (int  j = 0; j < 31; j++) {
+      if (tab32[i][j] == 0) {
+        display.drawBox(i * 3, j * 3, 3, 3);
+      };
+    };
+  };
+  display.drawPixel(4,4);
+  display.sendBuffer();          // transfer internal memory to the display
+};
+
+
 void setup() {
+  printf("hiii");
   Serial.begin(9600);
   display.begin();
   display.setPowerSave(0);
-  
+  tableau48();
+  Wire.setClock(400000);
   displaying();
 };
 
@@ -53,33 +82,3 @@ int InputDirections( int X, int Y) {
 
   return direction;
 };
-
-
-void displaying() {
-  int Tab = tableau48();
-  display.clearBuffer();          // clear the internal memory
-  for (int i = 0; i < 47; i++){
-    for (int j = 0; j<47;j++){
-      if (Tab[i][j]=0){
-        display.drawBox(i*2, j*2, 2, 2);
-      }
-    }
-  }
-  //display.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-//  display.drawBox(48, 48, 2, 2); // write something to the internal memory
-  display.sendBuffer();          // transfer internal memory to the display
-};
-
-int tableau48() {
-  int tab48[47][47] = {};
-  for (int i = 0; i < 47; i++) {
-    for (int j = 0; j < 47; j++) {
-      if ( i % 2 = 0 || j % 2 = 0 ) {
-        tab48[i][j] = 0;
-      } else {
-        tab48[i][j] = int(random(1, 20));
-      }
-    }
-  }
-  return tab48;
-} 
