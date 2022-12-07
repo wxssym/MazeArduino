60369fa30ccaefb972923325243f0a54541837df
wxssym <wassim.sisaid@gmail.com>
2022-12-07 19:35:01 +0100

    advancement till December 7

--- Projet/Projet.ino
+++ Projet/Projet.ino
@@ -8,6 +8,8 @@
 #define pinX    A1
 #define swPin    2
 int sensibilite = 256;
+int PlayerPosI = 1;
+int PlayerPosJ = 1;
 
 #define OLED_RESET -1
 unsigned int tab32[32][32];
@@ -33,33 +35,30 @@ void Generateur() {
 
     } else {
       if (tab32[x][y] == 0) {
-        switch (int axis = random(0, 2)) {
-          case 0:
-            if (tab32[x][y + 1] != tab32[x][y - 1] && tab32[x][y + 1] != 0 && tab32[x][y - 1] != 0  ) {
-              tab32[x][y] = tab32[x][y - 1];
-              A++;
-              for (int  i = 0; i < 31; i++) {
-                for (int  j = 0; j < 31; j++) {
-                  if ( tab32[i][j] == tab32[x][y + 1]) {
-                    tab32[i][j] = tab32[x][y - 1];
-                  }
+        if (random(0, 2) == 0) {
+          if (tab32[x][y + 1] != tab32[x][y - 1] && tab32[x][y + 1] != 0 && tab32[x][y - 1] != 0  ) {
+            tab32[x][y] = tab32[x][y - 1];
+            A++;
+            for (int  i = 0; i < 31; i++) {
+              for (int  j = 0; j < 31; j++) {
+                if ( tab32[i][j] == tab32[x][y + 1]) {
+                  tab32[i][j] = tab32[x][y - 1];
                 }
               }
             }
-            break;
-          case 1:
-            if (tab32[x + 1][y] != tab32[x - 1][y] && tab32[x + 1][y ] != 0 && tab32[x - 1][y] != 0 ) {
-              tab32[x][y] = tab32[x - 1][y];
-              A++;
-              for (int  i = 0; i < 31; i++) {
-                for (int  j = 0; j < 31; j++) {
-                  if ( tab32[i][j] == tab32[x + 1][y]) {
-                    tab32[i][j] = tab32[x - 1][y];
-                  }
+          }
+        } else {
+          if (tab32[x + 1][y] != tab32[x - 1][y] && tab32[x + 1][y ] != 0 && tab32[x - 1][y] != 0 ) {
+            tab32[x][y] = tab32[x - 1][y];
+            A++;
+            for (int  i = 0; i < 31; i++) {
+              for (int  j = 0; j < 31; j++) {
+                if ( tab32[i][j] == tab32[x + 1][y]) {
+                  tab32[i][j] = tab32[x - 1][y];
                 }
               }
             }
-            break;
+          }
         }
       }
     }
@@ -70,22 +69,18 @@ void Generateur() {
 
 
 U8G2_SH1107_SEEED_128X128_F_SW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
-//U8G2_SSD1327_SEEED_96X96_F_SW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
-//U8G2_SSD1327_SEEED_96X96_1_HW_I2C display(U8G2_R0, A5, A4);
-//Adafruit_SSD1327 display(96, 96, &Wire, OLED_RESET, 1000000);
-//U8X8_SSD1327_SEEED_96X96_SW_I2C u8x8(A5, A4);
 
 void displaying(int PlayerPosI, int PlayerPosJ) {
   display.clearBuffer();
   display.drawDisc(62, 62, 6, U8G2_DRAW_ALL); // clear the internal memory
-  for (int i = PlayerPosI - 3; i < PlayerPosI + 3 ; i++) {
-    for (int j = PlayerPosJ - 3; j < PlayerPosJ + 3 ; j++) {
+  for (int i = PlayerPosI - 2; i < PlayerPosI + 3 ; i++) {
+    for (int j = PlayerPosJ - 2; j < PlayerPosJ + 3 ; j++) {
       if (i < 0 || j < 0 || i > 32 || j > 32) {
 
       } else {
         if (tab32[i][j] == 0) {
           //display.drawBox(25 * (i+1) + 1, 25 * (j+1) + 1, 23, 23);
-          display.drawBox(25 * i + 1, 25 * j + 1, 23, 23);
+          display.drawBox(25 * (i - PlayerPosI + 2) + 1, 25 * (j - PlayerPosJ + 2) + 1, 23, 23);
         }
       }
     }
@@ -93,44 +88,96 @@ void displaying(int PlayerPosI, int PlayerPosJ) {
   display.sendBuffer();          // transfer internal memory to the display
 }
 
+
 void setup() {
-  int PlayerPosI = 3;
-  int PlayerPosJ = 1;
+
   randomSeed(analogRead(A7));
-  printf("hiii");
   Serial.begin(9600);
   display.begin();
   display.setPowerSave(0);
   Generateur();
-  //Wire.setClock(400000);
+  Wire.setClock(400000);
   displaying(PlayerPosI, PlayerPosJ);
 }
 
 void loop() {
-  int A = InputDirections(analogRead(pinX), analogRead(pinY));
-  Serial.println(analogRead(pinX));
+  //  int A = InputDirections(analogRead(pinX), analogRead(pinY));
+  //  Serial.println(A);
+  playerMouvement(InputDirections());
 }
 
+void playerMouvement(int playerInput) {
+  int tempPlayerPosJ;
+  int tempPlayerPosI;
+  switch (playerInput) {
+    case 1:
+      tempPlayerPosI = PlayerPosI + 1;
+      tempPlayerPosJ = PlayerPosJ;
+      if (!isItAWall(tempPlayerPosI, tempPlayerPosJ)) {
+        PlayerPosI = tempPlayerPosI;
+        PlayerPosJ = tempPlayerPosJ;
+        Serial.println("en droite");
+        displaying(PlayerPosI, PlayerPosJ);
+      }
+      break;
+    case 2:
+      tempPlayerPosI = PlayerPosI - 1;
+      tempPlayerPosJ = PlayerPosJ;
+      if (!isItAWall(tempPlayerPosI, tempPlayerPosJ)) {
+        PlayerPosI = tempPlayerPosI;
+        PlayerPosJ = tempPlayerPosJ;
+        Serial.println("en gauche");
+        displaying(PlayerPosI, PlayerPosJ);
 
+      }
+      break;
+    case 3:
+      tempPlayerPosI = PlayerPosI;
+      tempPlayerPosJ = PlayerPosJ + 1;
+      if (!isItAWall(tempPlayerPosI, tempPlayerPosJ)) {
+        PlayerPosI = tempPlayerPosI;
+        PlayerPosJ = tempPlayerPosJ;
+        Serial.println("en bas");
+        displaying(PlayerPosI, PlayerPosJ);
+      }
+      break;
+    case 4:
+      tempPlayerPosI = PlayerPosI;
+      tempPlayerPosJ = PlayerPosJ - 1;
+      if (!isItAWall(tempPlayerPosI, tempPlayerPosJ)) {
+        PlayerPosI = tempPlayerPosI;
+        PlayerPosJ = tempPlayerPosJ;
+        Serial.println("en haut");
+        displaying(PlayerPosI, PlayerPosJ);
+      }
+      break;
+  }
+}
 
+bool isItAWall(int tempPlayerPosI, int tempPlayerPosJ) {
+  if (tab32[tempPlayerPosI][tempPlayerPosJ] == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 //Fonction qui retourne la direction choisie par l'utilisateur
-int InputDirections( int X, int Y) {
+int InputDirections() {
   int direction = 0;
-
-  if (X >= 512 + sensibilite) {
-    Serial.println("A droite");
+  if (analogRead(pinX) >= 512 + sensibilite) {
+    //Serial.println("A droite");
     direction = 1;
-  } else if (X <= 512 - sensibilite) {
-    Serial.println("A gauche");
+  } else if (analogRead(pinX) <= 512 - sensibilite) {
+    //Serial.println("A gauche");
     direction = 2;
-  } else if (Y >= 512 + sensibilite) {
-    Serial.println("en bas");
+  } else if (analogRead(pinY) >= 512 + sensibilite) {
+    //Serial.println("en bas");
     direction = 3;
-  } else if (Y <= 512 - sensibilite) {
-    Serial.println("en haut");
+  } else if (analogRead(pinY) <= 512 - sensibilite) {
+    //Serial.println("en haut");
     direction = 4;
-  };
-
+  }
+  Serial.println(direction);
   return direction;
 };
